59a0b4686f3c4be7c30f4c7b597ea7e31dde87e6
Nickname5862 <nick.schuitemaker@hotmail.nl>
2022-09-23 19:37:48 +0200

    Step 1: reading some basic data

--- TestPML.py
+++ TestPML.py
@@ -1,14 +1,67 @@
+import matplotlib.pyplot as plt   # plots: for visualizing the data (e.g. histogram)
+import numpy as np
+import os                         # paths: for getting the files
 import pymeshlab
 ms = pymeshlab.MeshSet()
 
 
-base_path = r'C:\Users\Nickname5862\Desktop\UUmap\Multimedia Retrieval (MR)\labeledDb\LabeledDB_new\Airplane'
+base_path = os.getcwd() + '\labeledDb\LabeledDB_new'
+
+all_paths = []
+# Only add paths that lead to .off files: we don't want the labels.
+for (dirpath, dirnames, filenames) in os.walk(base_path):
+    for filename in filenames:
+        if filename.endswith('.off'):
+            all_paths.append(os.path.join(dirpath, filename))
+
+print(f"Found {len(all_paths)} .off files.")
+# Check whether all paths actually function, as a simple test.
+actual_paths = 0
+for path in all_paths:
+    actual_paths += 1 if os.path.exists(path) else 0
+print(f"Actual paths: {actual_paths} / {len(all_paths)}.")
+
+
+# https://pymeshlab.readthedocs.io/en/0.1.5/classes/mesh.html
+# For what a mesh all contains.
+
+def make_histogram(a, xlabel, ylabel, hist_step=200, x_min=0):
+    x_max = max(a)
+    x,y = np.histogram(a=a, bins=int((x_max-x_min)/hist_step), range=(x_min, x_max))
+    plt.stairs(x, y, fill=True)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
+
+def plot_stats():
+    hist_vertex = []
+    hist_face   = []
+    hist_edge   = []
+
+    for path in all_paths:
+        ms.load_new_mesh(path)
+        m = ms.current_mesh()
+
+        hist_vertex.append(m.vertex_number())
+        hist_face.append(m.face_number())    
+        hist_edge.append(m.edge_number()) 
+
+    make_histogram(hist_vertex,'No. of vertices','Frequency')
+    # make_histogram(hist_face,'No. of faces','Frequency')  # UNCOMMENT TO PRINT TOO
+    # make_histogram(hist_face,'No. of edges','Frequency')  # UNCOMMENT TO PRINT TOO
+
+
+plot_stats()
+
+
+
+quit() # Below here is just some previous test-code. Not important. "quit()" stops the code here.
+
+
 ms.load_new_mesh(base_path + r'\61.off')
 # ms.generate_convex_hull()
 ms.apply_filter('convex_hull')
 ms.save_current_mesh('convex_hull.ply')
-
-
 
 
 # https://pymeshlab.readthedocs.io/en/0.1.9/tutorials/load_mesh.html
