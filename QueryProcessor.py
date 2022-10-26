480e6422dc8c35f41cc24aeb3d7d6c1ec62b4c75
IsolatedSushi <s.vanherpt@students.uu.nl>
2022-10-26 16:33:01 +0200

    Update QueryProcessor.py

--- QueryProcessor.py
+++ QueryProcessor.py
@@ -5,32 +5,6 @@ from sklearn.neighbors import NearestNeighbors
 from termcolor import colored
 import random
 import time
-import faiss                   # make faiss available
-
-
-def getSortedNeighboursFaiss(queryModel, features, k=10):
-    data = []
-    classes = [getClassFromPath(path) for path in features]
-    # print(classes)
-    for filename in features:
-        row = features[filename]
-        values = list(row.values())
-        flat_list = values[:5] + [item for sublist in values[5:] for item in sublist]
-        data.append(flat_list)
-    data = np.array(data).astype('float32')
-
-    index = faiss.IndexFlatL2(data.shape[1])   # build the index
-    index.add(data)
-
-    D, I = index.search(data, k) # sanity check
-
-    fileNames = list(features.keys())
-    tuples = []
-    for i in range(len(D)):
-        tuples.append((D[i], fileNames[i]))
-
-    return 0
-
 
 def getSortedNeighbours(queryModel, features, k=10):
     
@@ -106,11 +80,7 @@ def main():
         features = json.load(read_content)
 
     k = 10
-    
-    # For checking how fast the querying is
-    # sampledVectors = getFeatureVectors(100000, features)
-    queryAllVectors(k, features)
-
+    getSortedNeighbours("models_final\\Airplane\\64.off", features, k)
 
 
 if __name__ == "__main__":
