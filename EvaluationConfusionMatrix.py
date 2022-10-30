d3cf1cd244ba04c17ea7af82d6296148878bf2d8
Nickname5862 <nick.schuitemaker@hotmail.nl>
2022-10-30 11:40:27 +0100

    Finished confusion-matrix, now with labels. Added distance-matrix for specific comparisons in EvaluationCM_specific (should have called it differently)

--- EvaluationConfusionMatrix.py
+++ EvaluationConfusionMatrix.py
@@ -66,45 +66,50 @@ def calculateConfusionMatrix(features, k=10):
 
 
 def main():
-    # with open("./database/normalized_features.json", "r") as read_content:
-    #     features = json.load(read_content)
-    # y_true, y_pred = calculateConfusionMatrix(features, k=5)
-    # labels = []
-    # [labels.append(x) for x in y_true if x not in labels]
-    # array = confusion_matrix(y_true, y_pred, labels=labels) # preferably y_true if all occur
+    with open("./database/normalized_features.json", "r") as read_content:
+        features = json.load(read_content)
+    y_true, y_pred = calculateConfusionMatrix(features, k=5)
+    labels = []
+    [labels.append(x) for x in y_true if x not in labels]
+    array = confusion_matrix(y_true, y_pred, labels=labels) # preferably y_true if all occur
 
-    # obtained from the code above, pasted manually to test different font sizes, etc.
-    # NOTE: THIS WAS y_pred ORDER, NOT y_true !! This means the order of labels and such may differ from the usual one
-    labels = ['Airplane', 'Bird', 'Table', 'Human', 'Plier', 'Ant', 'Hand', 'FourLeg', 'Armadillo', 'Octopus', 'Fish', 'Glasses', 'Cup', 'Bearing', 'Chair', 'Teddy', 'Mech', 'Bust', 'Vase']
-    array = [[148, 10, 1, 4,   11,   22,   2,   2,   0,  0,    0,   0,    0,   0,    0,    0,    0,   0,   0],
-            [14,  72,    6,  15,    8,   23,   9,  11,  12,   7,    4,  10,    0,   4 ,   5,    0,    0,   0,   0],
-            [15,  17,  105,   0,   12,    4,   2,   0,   5,   3,    0,  12,    7,   9 ,   5,    2,    0,   1,   1],
-            [10,   4,    0,  78,    4,   28,   9,   4,   8,   2,   50,   0,    0,   3 ,   0,    0,    0,   0,   0],
-            [16,   7,    0,  16,  157,    0,   0,   0,   0,   0,    0,   1,    0,   3 ,   0,    0,    0,   0,   0],
-            [9 ,  8 ,   0 , 14 ,   0 , 116 , 23 , 24 ,  4 ,  1 ,   1 ,  0 ,   0 ,  0  ,  0 ,   0 ,   0 ,  0 ,  0],
-            [7 ,  8 ,   0 ,  9 ,   0 ,  37 , 47 , 41 , 19 ,  6 ,  11 ,  0 ,   0 ,  0 ,   3 ,  12 ,   0 ,  0 ,  0],
-            [3 ,  8 ,   0 ,  3 ,   0 ,  30 , 41 , 79 ,  8 ,  3 ,   4 ,  0 ,   0 ,  0 ,   2 ,  18 ,   0 ,  0 ,  1],
-            [0 ,  3 ,   3 ,  7 ,   0 ,   2 , 16 ,  9 , 67 , 29 ,  13 ,  0 ,   1 ,  0 ,   8 ,  33 ,   0 ,  6 ,  3],
-            [6 , 18 ,   1 ,  3 ,   0 ,  10 , 25 , 10 , 45 , 37 ,   7 ,  0 ,   2 ,  1 ,  18 ,  13 ,   0 ,  3 ,  1],
-            [0 ,  0 ,   0 , 45 ,   0 ,   4 , 12 , 11 , 14 ,  2 , 108 ,  0 ,   0 ,  0 ,   0 ,   1 ,   0 ,  0 ,  3],
-            [10,  30,   26,   8,   18,    0,   4,   2,   0,   0,    4,  95,    1,   2,    0,    0,    0,   0,   0],
-            [0 ,  2 ,   6 ,  0 ,   0 ,   0 ,  1 ,  0 ,  4 ,  2 ,   0 ,  0 , 128 ,  0 ,   0 ,   0 ,  31 , 24 ,  2],
-            [0 , 11 ,   2 , 26 ,  11 ,  15 , 17 ,  9 ,  8 ,  2 ,   8 ,  3 ,   0 , 58 ,   2 ,  17 ,   0 ,  5 ,  6],
-            [0 ,  8 ,   2 ,  1 ,   0 ,   3 , 11 ,  7 , 12 ,  9 ,   0 ,  0 ,   0 ,  0 , 147 ,   0 ,   0 ,  0 ,  0],
-            [0 ,  0 ,   0 ,  0 ,   0 ,   0 ,  7 , 10 , 25 ,  2 ,   0 ,  0 ,   0 ,  0 ,   0 , 139 ,   0 , 14 ,  3],
-            [0 ,  0 ,   0 ,  0 ,   0 ,   0 ,  0 ,  0 ,  0 ,  0 ,   0 ,  0 ,  29 ,  0 ,   0 ,   0 , 136 , 26 ,  9],
-            [0 ,  0 ,   1 ,  0 ,   0 ,   0 ,  1 ,  2 ,  6 ,  4 ,   0 ,  0 ,  23 ,  4 ,   0 ,  32 ,  25 , 80 , 22],
-            [0 ,  0 ,   0 ,  0 ,   0 ,   1 , 10 ,  8 , 14 ,  5 ,   4 ,  0 ,   9 ,  3 ,   0 ,  31 ,  14 , 38 , 63]]
     # optionally: divide them all by 200 - 20 meshes in every category, k=10 for each.
     # note: only the rows or y-axis (y_real) sum to 200. The x-axis (y_pred) can sum to any number, as we can predict any class as much as desired
 
+#     0   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18
+# 0   88   7   0   0   0   0   0   3   0   0   0   0   0   2   0   0   0   0   0
+# 1    3  66   0  11   7   0   0   0   0   0   0   0   0   5   0   0   0   0   8
+# 2    0   0  78   0   1   0   0   6   3   0   0   0   1  11   0   0   0   0   0
+# 3    5  14   0  30   4   0   1   0   0   3   0  10   0   4   0   2   0   5  22
+# 4    4  11   0   4  48   0   1   0   0   0   0   7   0   2   0   0   0  22   1
+# 5    0   0   0   0   0  74   0   0   0   0   3   0   0   0  15   0   8   0   0
+# 6    3   7   0  10   3   0  25   0   1   2   1  25   1  10   1   7   0   2   2
+# 7    3   0   0   0   4   0   0  90   0   0   0   0   0   3   0   0   0   0   0
+# 8    9   2   4   0   0   0   1   4  61   1   1   2   2   5   1   2   5   0   0
+# 9    0   0   0   3   0   0   0   0   0  83   1   3   0   0   7   0   0   0   3
+# 10   0   1   0   4   0   6   3   0   0  13  36   6   2   0  23   0   1   3   2
+# 11   0   1   0   8   4   0  10   0   1   9   3  51   0   3   2   0   0   6   2
+# 12   0   4   0   7   8   0   1   4   0   5   2   4  50   5   4   0   0   6   0
+# 13   4  10   2   3   8   0   2   2   2   0   0   3   4  50   0   4   0   1   5
+# 14   0   0   0   0   0  13   1   0   0  17   9   3   2   0  48   0   7   0   0
+# 15   0   1   0   2   1   0   4   0   1   0   0   6   0   0   0  81   0   0   4
+# 16   0   0   0   0   0   8   2   0   3   0   1   1   0   0   8   0  77   0   0
+# 17   0   1   0   6  18   0   0   0   0   0   2   5   0   0   0   0   0  64   4
+# 18   1  10   0  24   2   0   3   0   0   8   1   1   0   2   0   1   0   1  46
+# ['Airplane', 'Ant', 'Glasses', 'Hand', 'Human', 'Mech', 'Octopus', 'Plier', 'Table', 'Teddy', 'Vase', 'Armadillo', 'Bearing', 'Bird', 'Bust', 'Chair', 'Cup', 'Fish', 'FourLeg']
+
+    labelsize = 0.6
+    size = 8
+
     x_dim = len(array)
     df_cm = pd.DataFrame(array, range(x_dim), range(x_dim))
-    sn.set(font_scale=1) # for label size
-    sn.heatmap(df_cm, cmap="blues", annot=True, annot_kws={"size": 10},fmt='d', xticklabels=labels, yticklabels=labels)
+    sn.set(font_scale=labelsize) # for label size
+    sn.heatmap(df_cm, cmap="Blues", annot=True, annot_kws={"size": size},fmt='d', xticklabels=labels, yticklabels=labels)
 
     print(df_cm)
     print(labels)
+    plt.xlabel("Predicted class")
+    plt.ylabel("Queried class")
 
     plt.show()
 
